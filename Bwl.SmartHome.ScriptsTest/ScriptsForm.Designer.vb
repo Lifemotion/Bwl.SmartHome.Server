49db77fc15fffe507a45fce8d992f5c5d25ee2cf
Igor Koshlelev <igor@lifemotion.ru>
2017-05-14 15:25:29 +0300

    (no message)

---
+++ Bwl.SmartHome.ScriptsTest/ScriptsForm.Designer.vb
@@ -0,0 +1,43 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ScriptsForm
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ScriptsForm))
+        Me.SuspendLayout()
+        '
+        'logWriter
+        '
+        Me.logWriter.Location = New System.Drawing.Point(2, 53)
+        '
+        'ScriptsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(517, 287)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "ScriptsForm"
+        Me.Text = "Bwl SmartHome Scripts"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+End Class
---
+++ Bwl.SmartHome.ScriptsTest/ScriptsForm.vb
@@ -0,0 +1,44 @@
+﻿Imports Bwl.Framework
+
+Public Class ScriptsForm
+    Inherits SmartHomeClientBase
+
+    Private Sub ComputerControlApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler _client.SmartHome.Objects.StateChanged, AddressOf StateChangedHandler
+        Text += " " + Application.ProductVersion
+        niTray.Text = "BWL SH: Scripts"
+        niTray.Icon = Me.Icon
+#If Not DEBUG Then
+        Dim invisible As New Threading.Thread(Sub() Me.Invoke(Sub() Hide()))
+        invisible.Start()
+#End If
+    End Sub
+
+    Private Sub SendObjectsTimerHandler()
+        Try
+            '     _client.SmartHome.Objects.SetScheme(_guid, _computerObjectScheme)
+        Catch ex As Exception
+            _logger.AddWarning(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub StateChangedHandler(objGuid As String, stateId As String, lastValue As String, currentValue As String, changedBy As ChangedBy)
+        If objGuid = "da72bbb9-7167-49c9-9949-6f35b9bf1140" Then
+            If stateId = "SwitchAll" And currentValue = "off" Then
+                Dim thread As New Threading.Thread(Sub()
+                                                       Try
+                                                           _client.SmartHome.Objects.SetValue("daa32c18-e0e1-4bf0-ab09-25f1e0753f1f", "RGB1Switch", "on", ChangedBy.script)
+                                                       Catch ex As Exception
+                                                       End Try
+                                                       Threading.Thread.Sleep(10000)
+                                                       Try
+                                                           _client.SmartHome.Objects.SetValue("daa32c18-e0e1-4bf0-ab09-25f1e0753f1f", "RGB1Switch", "off", ChangedBy.script)
+                                                       Catch ex As Exception
+                                                       End Try
+                                                   End Sub)
+                thread.Start()
+            End If
+        End If
+    End Sub
+
+End Class
